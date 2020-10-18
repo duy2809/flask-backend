ed26f00f120fa074dab5481d8ebe36f64306bfb1
Duy Anh <duyanhvn1999@gmail.com>
2020-10-18 11:08:02 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,64 @@
+import io
+import json
+import uuid
+import datetime
+
+from torchvision import models
+import torchvision.transforms as transforms
+from PIL import Image
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello():
+#     return 'Hello World!'
+
+imagenet_class_index = json.load(
+    open('./imagenet_class_index.json'))
+model = models.densenet121(pretrained=True)
+model.eval()
+
+
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    predicted_idx = str(y_hat.item())
+    return imagenet_class_index[predicted_idx]
+
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     if request.method == 'POST':
+#         file = request.files['file']
+#         img_bytes = file.read()
+#         class_id, class_name = get_prediction(image_bytes=img_bytes)
+#         return jsonify({'class_id': class_id, 'class_name': class_name})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        data = request.data.decode('utf-8')
+        dict = json.loads(data)
+        message_user = dict["text"]
+        return jsonify({
+        "text": message_user,
+        "createdAt": datetime.datetime.now(),
+        "user": { "_id": "2", "name": "Placeholder", "avatar": "https://placeimg.com/140/140/any" },
+        "_id": uuid.uuid4()})
+
+if __name__ == '__main__':
+    app.run() #ngrok
